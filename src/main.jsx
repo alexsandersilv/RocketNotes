a7e0615bdec0dc6501cd7f5f179c73f47826a8d2
Alexsander <81785712+alexsandersilv@users.noreply.github.com>
2023-02-20 17:10:51 -0300

    signin page finish

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Home } from './pages/Home';
+import { SignIn } from './pages/SingIn';
 import { Details } from './pages/Details';
+
 
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
@@ -13,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,37 @@
+import { Container, Form, Background } from './style';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button'
+
+import { FiMail, FiLock } from 'react-icons/fi'
+
+export function SignIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>
+          Aplicação para salvar e gerenciar seus links uteis
+        </p>
+
+        <h2>Faça seu login</h2>
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icone={FiMail}
+        />
+
+        <Input
+          placeholder="Senha"
+          type="passsword"
+          icone={FiLock}
+        />
+
+        <Button title="entrar" />
+        <a href="#">Criar Conta</a>
+      </Form>
+      <Background />
+    </Container>
+  );
+}
