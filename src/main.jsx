5194e192575d82f729307eeb1b42df24880319ed
Alexsander <81785712+alexsandersilv@users.noreply.github.com>
2023-02-22 08:24:50 -0300

    new note page finish

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { SignUp } from './pages/SignUp';
 import { Home } from './pages/Home';
 import { Details } from './pages/Details';
 import { Profile } from './pages/Profile';
-
+import { New } from './pages/New';
 
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/New/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Form } from './style';
+
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+
+export function New(){
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Titulo" />
+          <Textarea placeholder="Observações" />
+        </Form>
+      </main>
+    </Container>
+  )
+}
