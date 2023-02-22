0f86e7d61f82f91216deafe2dcfdd0695fe60601
Alexsander <81785712+alexsandersilv@users.noreply.github.com>
2023-02-22 09:07:44 -0300

    adding components to new note page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,8 +3,12 @@ import { Container, Form } from './style';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
-export function New(){
+
+export function New() {
   return (
     <Container>
       <Header />
@@ -18,6 +22,28 @@ export function New(){
 
           <Input placeholder="Titulo" />
           <Textarea placeholder="Observações" />
+          <Section title="Links">
+            <NoteItem
+              value="https://github.com/alexsandersilv"
+            />
+            <NoteItem
+              placeholder="Novo Link"
+              isNew
+            />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem
+                value="react"
+              />
+              <NoteItem
+                placeholder="Nova tag"
+                isNew
+              />
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
